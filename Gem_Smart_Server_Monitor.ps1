a0a50801d5516f2ca6a2750a887f348fd41621f1
SMControl <143865768+SMControl@users.noreply.github.com>
2024-05-14 20:14:54 +0100

    Update Gem_Smart_Server_Monitor.ps1

--- Gem_Smart_Server_Monitor.ps1
+++ Gem_Smart_Server_Monitor.ps1
@@ -1,17 +1,30 @@
-# Set the directory where SmartServer.exe resides
+# ===========================================================================
+# Section 1: Adding Script to Startup Folder (Requires Administrator Privileges)
+
+$scriptDir = (Get-Location).Path
+$scriptFile = (Get-ScriptFilename)
+$startupFolder = "$env:USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup"
+$shortcutPath = Join-Path $startupFolder -ChildPath "$scriptFile.lnk"
+
+if (!(Test-Path $shortcutPath)) {
+  if (!(Test-Path $startupFolder)) {
+    New-Item -ItemType Directory -Path $startupFolder -ErrorAction SilentlyContinue
+  }
+  New-Item -ItemType SymbolicLink -Path $shortcutPath -Target $scriptPath -Force -ErrorAction SilentlyContinue
+}
+
+# ===========================================================================
+# Section 2: Monitoring and Restarting SmartServer.exe (Silent)
+
 $smartServerDir = "C:\SmartServer"
 
-# Create a function to check if SmartServer.exe is running
 function IsSmartServerRunning {
   Get-Process | Where-Object { $_.Name -eq "SmartServer.exe" } | Out-Null
 }
 
-# Continuously check and restart SmartServer.exe every 30 seconds
 while ($true) {
-  # Check if SmartServer is running
   if (!(IsSmartServerRunning)) {
-    Write-Host "SmartServer.exe is not running. Restarting..."
-    Start-Process -FilePath "$smartServerDir\SmartServer.exe" -WorkingDirectory $smartServerDir -Wait
+    Start-Process -FilePath "$smartServerDir\SmartServer.exe" -WorkingDirectory $smartServerDir -Wait -ErrorAction SilentlyContinue
   }
-  Start-Sleep -Seconds 30 # Wait 30 seconds before checking again
+  Start-Sleep -Seconds 30
 }
