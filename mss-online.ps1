6d20d18cbad18469c499c088db9a39b02b326540
SMControl <143865768+SMControl@users.noreply.github.com>
2024-05-15 22:05:50 +0100

    Update mss-online.ps1

--- mss-online.ps1
+++ mss-online.ps1
@@ -8,12 +8,23 @@ New-Item -ItemType Directory -Path (Split-Path $destinationPath) -Force | Out-Nu
 # Download the file
 (New-Object System.Net.WebClient).DownloadFile($downloadUrl, $destinationPath)
 
-# Create Scheduled Task
+################################
+# Part 6 - Create Scheduled Task
+################################
+
+# Define the trigger for the task to run at system startup
 $trigger = New-ScheduledTaskTrigger -AtStartup
+
+# Define the action for the task to execute the downloaded executable
 $action = New-ScheduledTaskAction -Execute "C:\SmartServer\MonitorSmartServer.exe" -WorkingDirectory "C:\SmartServer"
-$currentUser = (Get-User).Name  # Get current user name
-$settings = New-ScheduledTaskSettingsSet -RunAs $currentUser -Hidden
 
-Register-ScheduledTask -TaskName "SP_Monitor_SmartServer" -Trigger $trigger -Action $action -Settings $settings
+# Define the current user
+$currentUser = $env:USERNAME
 
-Write-Host "Task 'SP_Monitor_SmartServer' created and configured for system startup (hidden)!"
+# Define the settings for the task
+$settings = New-ScheduledTaskSettingsSet -RunOnlyIfLoggedOn -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopIfGoingOnBatteries -WakeToRun -RunOnlyIfNetworkAvailable
+
+# Register the scheduled task
+Register-ScheduledTask -TaskName "SP_Monitor_SmartServer" -Trigger $trigger -Action $action -User $currentUser -Settings $settings
+
+Write-Host "Task 'SP_Monitor_SmartServer' created and configured for system startup!"
