e5a8352ee6ee724924c5ea48e065ace16d9f999f
SMControl <143865768+SMControl@users.noreply.github.com>
2024-05-15 22:14:31 +0100

    Update mss-online.ps1

--- mss-online.ps1
+++ mss-online.ps1
@@ -1,16 +1,13 @@
 # Download MonitorSmartServer.exe
-$downloadUrl="https://raw.githubusercontent.com/SMControl/SM_SS/main/MonitorSmartServer.exe"
-$destinationPath="C:\SmartServer\MonitorSmartServer.exe"
-
-# Create directory if it doesn't exist
-New-Item -ItemType Directory -Path (Split-Path $destinationPath) -Force|Out-Null
+$downloadUrl = "https://raw.githubusercontent.com/SMControl/SM_SS/main/MonitorSmartServer.exe"
+$destinationPath = "C:\SmartServer\MonitorSmartServer.exe"
 
 # Download the file
-(New-Object System.Net.WebClient).DownloadFile($downloadUrl,$destinationPath)
+(New-Object System.Net.WebClient).DownloadFile($downloadUrl, $destinationPath)
 
 # Create Scheduled Task
-$trigger=New-ScheduledTaskTrigger -AtLogon
-$action=New-ScheduledTaskAction -Execute "C:\SmartServer\MonitorSmartServer.exe" -WorkingDirectory "C:\SmartServer"
+$trigger = New-ScheduledTaskTrigger -AtLogon
+$action = New-ScheduledTaskAction -Execute "C:\SmartServer\MonitorSmartServer.exe" -WorkingDirectory "C:\SmartServer"
 Register-ScheduledTask -TaskName "SP_Monitor_SmartServer" -Trigger $trigger -Action $action
 
-Write-Host "Task 'SP_Monitor_SmartServer' created and configured for system startup!"
+Write-Host "Task 'SP_Monitor_SmartServer' created and configured to run at user logon!"
