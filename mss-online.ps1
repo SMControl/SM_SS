b3b54011b4c06eb6e5963c4306ba599d67da5c94
SMControl <143865768+SMControl@users.noreply.github.com>
2024-05-15 22:09:15 +0100

    Update mss-online.ps1

--- mss-online.ps1
+++ mss-online.ps1
@@ -1,30 +1,18 @@
 # Download MonitorSmartServer.exe
-$downloadUrl = "https://raw.githubusercontent.com/SMControl/SM_SS/main/MonitorSmartServer.exe"
-$destinationPath = "C:\SmartServer\MonitorSmartServer.exe"
+$downloadUrl="https://raw.githubusercontent.com/SMControl/SM_SS/main/MonitorSmartServer.exe"
+$destinationPath="C:\SmartServer\MonitorSmartServer.exe"
 
 # Create directory if it doesn't exist
-New-Item -ItemType Directory -Path (Split-Path $destinationPath) -Force | Out-Null
+New-Item -ItemType Directory -Path (Split-Path $destinationPath) -Force|Out-Null
 
 # Download the file
-(New-Object System.Net.WebClient).DownloadFile($downloadUrl, $destinationPath)
+(New-Object System.Net.WebClient).DownloadFile($downloadUrl,$destinationPath)
 
-################################
-# Part 6 - Create Scheduled Task
-################################
-
-# Define the trigger for the task to run at system startup
-$trigger = New-ScheduledTaskTrigger -AtStartup
-
-# Define the action for the task to execute the downloaded executable
-$action = New-ScheduledTaskAction -Execute "C:\SmartServer\MonitorSmartServer.exe" -WorkingDirectory "C:\SmartServer"
-
-# Define the current user
-$currentUser = $env:USERNAME
-
-# Define the settings for the task
-$settings = New-ScheduledTaskSettingsSet -RunOnlyIfLoggedOn -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopIfGoingOnBatteries -WakeToRun -RunOnlyIfNetworkAvailable
-
-# Register the scheduled task
+# Create Scheduled Task
+$trigger=New-ScheduledTaskTrigger -AtStartup
+$action=New-ScheduledTaskAction -Execute "C:\SmartServer\MonitorSmartServer.exe" -WorkingDirectory "C:\SmartServer"
+$currentUser=$env:USERNAME
+$settings=New-ScheduledTaskSettingsSet -RunOnlyIfLoggedOn -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopIfGoingOnBatteries -WakeToRun -RunOnlyIfNetworkAvailable
 Register-ScheduledTask -TaskName "SP_Monitor_SmartServer" -Trigger $trigger -Action $action -User $currentUser -Settings $settings
 
 Write-Host "Task 'SP_Monitor_SmartServer' created and configured for system startup!"
